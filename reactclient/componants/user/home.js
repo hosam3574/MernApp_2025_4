6bfeb916c8ee52ebe34ecfd3db2e4067d797586d
hosam3574 <hossamtradat@gmail.com>
2025-12-27 20:52:03 +0300

    save

---
+++ reactclient/componants/user/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Home = () => {
+    const { user } = useAuth();
+    console.log("User data in Home:", user);
+
+  return (
+    <div>
+      <h1>Welcome to the User Home Page</h1>
+        <h2>Hello, {user ? user.username : 'Guest'}!</h2>
+      <p>This is the home page for users.</p>
+    </div>
+  );
+};
+
+export default Home;
