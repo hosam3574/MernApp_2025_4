c1701d4782c42029de5077881bc132b1d10f125c
hosam3574 <hossamtradat@gmail.com>
2025-12-27 23:59:45 +0300

    save

--- reactclient/services/auth.js
+++ reactclient/services/auth.js
@@ -1,10 +1,16 @@
+//  
 import api from "./api";
-export async function loginForm(email,password){
+export async function login(email,password){
 
     const {data} = await api.post('users/login', { email, password });
     localStorage.setItem('token', data.token);
     localStorage.setItem('user', JSON.stringify(data.user));
 
+    return data;
+}
+export async function register(username, email, password) {
+    const { data } = await api.post('users/create', { username, email, password });
+    localStorage.setItem('user', JSON.stringify(data.user));
     return data;
 }
 export async function logout(){
@@ -13,9 +19,9 @@ export async function logout(){
 }
 //  regester 
 
-export async function getCurrentUSer(){
+export  function getCurrentUSer(){
     const raw = localStorage.getItem('user');
     if(!raw) return null
-    const user = JSON.parse(raw)
+    const user =  JSON.parse(raw)
     return user
 }
--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './componants/login';
 import Home from './componants/user/home';
 import {BrowserRouter  ,Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './componants/ProtectedRoute';
+import Register from './componants/register';
+import { useAuth } from './context/AuthContext';
+import Dashboard from './componants/admin/dashboard';
+import { useEffect } from 'react';
+import AdminRoute from './componants/AdminRoute';
 
 
- 
+
 function App() {
+  //  we will declatre const called user if the user is admin then the home route will display the dashboard componant 
+  //  else if the user role is user then it will display the user home componant
+  const {user} = useAuth();
+  console.log("User data in App:", user);
+useEffect(() => {
+  document.title = "Mern App 2025";
+}, []);
   return (
     <div className="App">
-     
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Login/>}/>
-            <Route path='/home' element={<Home/>}/>
+            {/* map  */}
+
+            <Route path='/login' element={<Login/>}/>
+            {/*                       null   */}
+            <Route path='/home' element={user?.role=== 'admin' ? <AdminRoute><Dashboard/></AdminRoute> : <ProtectedRoute><Home/></ProtectedRoute> }/>
+            <Route path='/register' element={<Register/>}/>
+            {/* 404 */}
+            <Route path='*' element={<h1>404 - Not Found</h1>} />
           </Routes>
         </BrowserRouter>
-     
     </div>
   );
 }
